1bd0b45c7f60dc5cba2af016568004b7e672fa35
sahinmuratr <sahinmuratr@gmail.com>
2023-10-11 12:43:38 +0300

    Add project files.

---
+++ emnayca/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace emnayca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello, World!");
+            Console.WriteLine("Merhabayın gencler");
+
+        }
+    }
+}
